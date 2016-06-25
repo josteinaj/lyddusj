2509c071de194d52cf2a6c9ed30da853db95e6f8
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2016-06-25 17:18:20 +0200

    add support for jingle (default off)

--- lyddusj.sh
+++ lyddusj.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "$JINGLE" != "1" ]; then
+    JINGLE="0"
+fi
+
 if [ "`ps aux | grep -v grep | grep /tmp/audio.mp3 | wc -l`" = "1" ]; then
     echo "already playing"
     exit
@@ -23,5 +27,8 @@ while [ 1 ]; do
     sleep 2
     wget http://beta.nlb.no/baksidetekst/$BOOK_ID.mp3 -O /tmp/audio.mp3
     play /tmp/audio.mp3
+    if [ "$JINGLE" = "1" ]; then
+        play "$DIR/NLB.mp3"
+    fi
     sleep 10
 done
